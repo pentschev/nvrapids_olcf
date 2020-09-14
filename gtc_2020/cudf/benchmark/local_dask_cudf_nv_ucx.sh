4b8eac193fd7d375f6f619e39ccce44b80a9ae34
Peter Andreas Entschev <peter@entschev.com>
2020-09-14 06:22:47 -0700

    Add local benchmark scripts

---
+++ gtc_2020/cudf/benchmark/local_dask_cudf_nv_ucx.sh
@@ -0,0 +1,22 @@
+NUM_WORKERS=6
+PACKAGE="dask_cudf"
+SCRIPT="local_cudf_benchmarking_nv.py"
+SCHEDULER_JSON="/tmp/dask/my-scheduler-gpu.json"
+
+CONDA_ENV_DIR=/datasets/pentschev/miniconda3/envs/r-102-0.15
+
+export DASK_UCX__CUDA_COPY=True
+export DASK_UCX__NVLINK=True
+export DASK_UCX__INFINIBAND=False
+#export DASK_UCX__INFINIBAND=True
+export DASK_UCX__NET_DEVICES=ib0
+
+# I/O - TESTING CHUNKING
+
+#python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G --read_chunk_mb 4096 --stop_at_read True
+python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G --stop_at_read True
+
+# Partitioning
+
+python $SCRIPT --package $PACKAGE --num_dask_workers $NUM_WORKERS --scheduler_json_path $SCHEDULER_JSON --file_size 25G
+
